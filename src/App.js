a0539bc567fbd7600cc2f947f4b213d2845311f6
Brandon Castro <castroyou999@gmail.com>
2023-01-04 16:56:46 -0500

    commit tree 5072a43d8497cc013903591d1a305b29bdf43cd5

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import React, { useState, useEffect } from "react";
 
 function App() {
+
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+
+  useEffect(
+    // 1st arg: side effect (callback function)
+  () => console.log("useEffect called"),
+  // 2nd arg: dependencies array
+  [count]
+  );
+
   useEffect(() => {
-    console.log("useEffect called");
-  });
+    document.title = text;
+  }, [text])
+
+  useEffect(() => {
+    setTimeout(() => setCount(0), 3000);
+  }, [count]);
 
   console.log("Component rendering");
 
-  return <button>Click Me</button>;
+  return (
+    <div>
+      <button onClick={() => setCount((count) => count + 1)}>
+        I've been clicked {count} times
+      </button>
+      <input 
+        type="text" 
+        value={text}
+        onChange={(event)=> setText(event.target.value)}
+      />
+    </div>
+  );
 }
 
 export default App;
